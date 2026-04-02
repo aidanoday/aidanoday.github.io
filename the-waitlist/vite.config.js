8028205b47aeeafc0cd30ba1a15c3a5c71afe2d8
Aidan <aidanoday@gmail.com>
2026-04-02 01:16:04 -0700

    deploying waitlist and modifying vite build config

--- the-waitlist/vite.config.js
+++ the-waitlist/vite.config.js
@@ -5,5 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/thewaitlist/',
+  build: {
+    // Tells Vite to output the files one folder up, into "thewaitlist"
+    outDir: '../thewaitlist', 
+    
+    // Tells Vite to safely clear the old files out of that folder before putting the new ones in
+    emptyOutDir: true 
+  }
 })
 
